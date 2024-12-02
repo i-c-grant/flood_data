d82fb43c08332d862cf8bec6a7bb5a642c3a02a1
Ian Grant <ian.conway.grant@gmail.com>
2024-12-02 11:32:53 -0500

    added abstract BaseClient

---
+++ src/clients/BaseClient.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Optional, Union
+import polars as pl
+import geopandas as gpd
+
+
+class BaseClient(ABC):
+    @abstractmethod
+    def get_data(
+        self,
+        spatial_filter: Optional[gpd.GeoSeries] = None,
+        temporal_filter: Optional[Union[datetime, tuple[datetime, datetime]]] = None,
+    ) -> pl.DataFrame:
+        """Fetch data with optional spatial and temporal filtering
+
+        Args:
+            spatial_filter: GeoSeries containing one or more geometries to filter by
+            temporal_filter: Single datetime for point-in-time or tuple of datetimes for range
+
+        Returns:
+            DataFrame containing the filtered data
+        """
+        pass
